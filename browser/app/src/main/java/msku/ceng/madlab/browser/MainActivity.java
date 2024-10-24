03c7eef7faa5f26b12d0c5e5b73b3666d0de57df
u210709043 <yusufkoksal@posta.mu.edu.tr>
2024-10-24 11:09:50 +0300

    lab 4

---
+++ browser/app/src/main/java/msku/ceng/madlab/browser/MainActivity.java
@@ -0,0 +1,50 @@
+package msku.ceng.madlab.browser;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnGo;
+    EditText txtAddress;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGo = findViewById(R.id.go);
+        txtAddress = findViewById(R.id.adress);
+        webView = findViewById(R.id.webview);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.loadUrl("http://"+ txtAddress.getText());
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
