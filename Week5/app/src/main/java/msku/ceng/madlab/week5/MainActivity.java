2b72a9bdb64dbcc74baac2e510918414d207f606
u210709043 <yusufkoksal@posta.mu.edu.tr>
2024-10-31 11:25:10 +0300

    week5 second

--- Week5/app/src/main/java/msku/ceng/madlab/week5/MainActivity.java
+++ Week5/app/src/main/java/msku/ceng/madlab/week5/MainActivity.java
@@ -1,6 +1,7 @@
 package msku.ceng.madlab.week5;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
@@ -8,6 +9,7 @@ import android.widget.TableRow;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     boolean player1Turn = true;
 
     byte[][] board = new byte[3][3];
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +65,93 @@ public class MainActivity extends AppCompatActivity {
                 board[row][col] = 2;
             }
 
+            if (gameEnded(row, col) == -1) {
+                player1Turn = !player1Turn;
+            } else if (gameEnded(row, col) == 0) {
+                Toast.makeText(MainActivity.this, "It is a draw", Toast.LENGTH_LONG).show();
+            } else if (gameEnded(row, col) == 1) {
+                Toast.makeText(MainActivity.this, "Player 1 wins!", Toast.LENGTH_LONG).show();
+            } else if (gameEnded(row, col) == 2) {
+                Toast.makeText(MainActivity.this, "Player 2 wins!", Toast.LENGTH_LONG).show();
+            }
+
         }
+
+        public int gameEnded(int row, int col) {
+            int symbol = board[row][col];
+            boolean win = true;
+
+            // Check the column
+            for (int i = 0; i < 3; i++) {
+                if (board[i][col] != symbol) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                return symbol;
+            }
+
+            // Check the row
+            win = true;
+            for (int j = 0; j < 3; j++) {
+                if (board[row][j] != symbol) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                return symbol;
+            }
+
+            // Check the main diagonal (top-left to bottom-right)
+            if (row == col) {
+                win = true;
+                for (int i = 0; i < 3; i++) {
+                    if (board[i][i] != symbol) {
+                        win = false;
+                        break;
+                    }
+                }
+                if (win) {
+                    return symbol;
+                }
+            }
+
+            // Check the anti-diagonal (top-right to bottom-left)
+            if (row + col == 2) {
+                win = true;
+                for (int i = 0; i < 3; i++) {
+                    if (board[i][2 - i] != symbol) {
+                        win = false;
+                        break;
+                    }
+                }
+                if (win) {
+                    return symbol;
+                }
+            }
+
+            // Check for a draw (no empty cells left)
+            boolean draw = true;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (board[i][j] == 0) { // Assuming 0 represents an empty cell
+                        draw = false;
+                        break;
+                    }
+                }
+                if (!draw) {
+                    break;
+                }
+            }
+            if (draw) {
+                return 0; // Game is a draw
+            }
+
+            return -1; // Game is ongoing
+        }
+
 
         public boolean isValidMove(int row,int col){
 
@@ -70,5 +160,39 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        outState.putBoolean("player1Turn",player1Turn);
+        byte[] boardSingle = new byte[9];
+        for (int i = 0; i < 3 ; i++) {
+            for (int j = 0; j < 3; j++) {
+                boardSingle[3 * i + j] = board[i][j];
+            }
+        }
+        outState.putByteArray("board",boardSingle);
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
 
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        player1Turn = savedInstanceState.getBoolean("player1Turn");
+        byte[] boardSingle = savedInstanceState.getByteArray("board");
+        for (int i = 0; i < 9 ; i++) {
+            board[i/3][i%3] = boardSingle[i];
+        }
+
+        TableLayout table = findViewById(R.id.board);
+        for (int i = 0; i < 3; i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            for (int j = 0; j < 3; j++) {
+                Button button = (Button) row.getChildAt(j);
+                if(board[i][j] == 1){
+                    button.setText("X");
+                } else if (board[i][j] == 2) {
+                    button.setText("O");
+                }
+            }
+        }
+    }
 }
